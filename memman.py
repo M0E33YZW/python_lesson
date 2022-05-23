f3311d731c8924f21fc3dc98f081491fd293741c
nyanmy <nyanmyth100@gmail.com>
2022-05-23 19:09:59 +0900

    testに向けた修正

--- memman.py
+++ memman.py
@@ -1,24 +1,31 @@
+MIN_VAL = -10000 # -∞(最小)
+MAX_VAL =  10000 #  ∞(最大)
+
 class Memman:
 
     # コンストラクタ
     # 引数 self レシーバ
     # 戻値 なし
     def __init__(self):
-        self.MIN_VAL = -10000 # -∞(最小)
-        self.MAX_VAL =  10000 #  ∞(最大)
     
         # 空リスト中の現在の組の数
         self.num = 1
 
         # 始点配列（要素数100）
         self.spos = [0] * 100
-        self.spos[0] = self.MIN_VAL
-        # print(self.spos)
+        self.spos[0] = MIN_VAL
 
         # 終点配列（要素数100）
         self.epos = [0] * 100
-        self.epos[1] = self.MAX_VAL
-        # print(self.epos)
+        self.epos[0] = MAX_VAL
+
+    @classmethod
+    def init_for_unittest(cls, num, spos, epos):
+        m = cls()
+        m.num = num
+        m.spos = spos
+        m.epos = epos
+        return m
 
 
     # 引数 self レシーバ
@@ -26,20 +33,20 @@ class Memman:
     #     int: ep(終点P)
     # 戻値 なし
     def Alloc(self, sp, ep):
-        i = 1
+        i = 0
 
-        print('終点P > 終点[I]')
-        print(ep, '>', self.epos[i])
+        # print('終点P > 終点[I]')
+        # print(ep, '>', self.epos[i])
         while ep > self.epos[i]:
             i += 1
 
         if self.spos[i] <= sp:
-            print('始点[I] <= 始点P')
-            print(self.spos[i], '<=', sp)
+            # print('始点[I] <= 始点P')
+            # print(self.spos[i], '<=', sp)
 
             if self.spos[i] == sp and ep == self.epos[i]:
-                print('①(始点[I] == 始点P) かつ (終点P == 終点[I])')
-                print(self.spos[i], '==', sp, 'and', ep, '==', self.epos[i])
+                # print('①(始点[I] == 始点P) かつ (終点P == 終点[I])')
+                # print(self.spos[i], '==', sp, 'and', ep, '==', self.epos[i])
 
                 l = i + 1
                 while (l <= self.num):
@@ -49,18 +56,18 @@ class Memman:
                 self.num = self.num - 1
 
             elif self.spos[i] == sp and ep < self.epos[i]:
-                print('②(始点[I] == 始点P) かつ (終点P < 終点[I])')
-                print(self.spos[i], '==', sp, 'and', ep, '<', self.epos[i])
+                # print('②(始点[I] == 始点P) かつ (終点P < 終点[I])')
+                # print(self.spos[i], '==', sp, 'and', ep, '<', self.epos[i])
                 self.spos[i] = ep + 1
 
             elif self.spos[i] < sp and ep == self.epos[i]:
-                print('③(始点[I] < 始点P) かつ (終点P == 終点[I])')
-                print(self.spos[i], '<', sp, 'and', ep, '==', self.epos[i])
+                # print('③(始点[I] < 始点P) かつ (終点P == 終点[I])')
+                # print(self.spos[i], '<', sp, 'and', ep, '==', self.epos[i])
                 self.epos[i] = sp - 1
 
             elif self.spos[i] < sp and ep < self.epos[i]:
-                print('④(始点[I] < 始点P) かつ (終点P < 終点[I])')
-                print(self.spos[i], '<', sp, 'and', ep, '<', self.epos[i])
+                # print('④(始点[I] < 始点P) かつ (終点P < 終点[I])')
+                # print(self.spos[i], '<', sp, 'and', ep, '<', self.epos[i])
                 l = self.num
                 while (l >= i + 1):
                     self.spos[l + 1] = self.spos[l]
@@ -71,15 +78,24 @@ class Memman:
                 self.epos[i + 1] = self.epos[i]
                 self.epos[i] = sp - 1
                 self.num += 1
-
-            print(self.spos)
-            print(self.epos)
-            print('空リスト中の組数', self.num)
-
+            
         else:
-            print("一部又は全体が割当済み")
+            raise Exception("一部又は全体が割当済み")
 
-m = Memman()
-m.Alloc(1, 3)
-m.Alloc(5, 7)
-m.Alloc(2, 4)
+    def print(self):
+        print(self.spos)
+        print(self.epos)
+        print('空リスト中の組数', self.num)
+
+
+# m = Memman()
+# m.print()
+# m.Alloc(1, 3)
+# m.print()
+# m.Alloc(5, 7)
+# m.print()
+
+# try:
+#     m.Alloc(2, 4)
+# except Exception as e:
+#     print('例外を補足：', e)
