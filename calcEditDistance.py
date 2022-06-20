3be35c46c8de903b080fb60bd3273f852ba8e5d3
nyanmy <nyanmyth100@gmail.com>
2022-06-20 17:12:00 +0900

    リスト内包表記

--- calcEditDistance.py
+++ calcEditDistance.py
@@ -1,10 +1,9 @@
 class EditDistance:
 
     @classmethod
-    def init_for_unittest(cls, str1, str2):
+    def init_for_unittest(cls, D):
         e = cls()
-        e.str1 = str1
-        e.str2 = str2
+        e.D = D
         return e
 
     # 引数 str: str1 変換元の文字列が格納されている1次元配列
@@ -15,11 +14,14 @@ class EditDistance:
         str1len = len(str1) # 変換元の文字列の長さ（1以上）
         str2len = len(str2) # 変換先の文字列の長さ（1以上）
 
-        D = [0 for _ in range(str1len + 1)]
+        # リスト内包表記
+        # D = [0 for _ in range(str1len + 1)]
+        # for i, _ in enumerate(D):
+        #     D[i] = [0 for _ in range(str2len + 1)]
 
-        for i, _ in enumerate(D):
-            D[i] = [0 for _ in range(str2len + 1)]
-        # print('二次元配列初期化：', D)
+        # リスト内包表記のリスト内包表記
+        D = [[0 for _ in range(0, str2len + 1)] for _ in range(0, str1len + 1)]
+        print('二次元配列初期化：', D)
 
         x = 0
         while (x <= str1len):
@@ -48,10 +50,12 @@ class EditDistance:
 e = EditDistance
 print(e.calcEditDistance('abcabba', 'cbabac'))
 # print('')
-# print(calcEditDistance('abcabba', 'abcabba'))
+# print(e.calcEditDistance('abcabba', 'abcabba'))
 # print('')
-# print(calcEditDistance('abcabba', 'aabcabba'))
+# print(e.calcEditDistance('abcabba', 'aabcabba'))
 # print('')
-# print(calcEditDistance('abcabba', 'acabba'))
+# print(e.calcEditDistance('abcabba', 'acabba'))
 # print('')
-# print(calcEditDistance('abcabba', 'abcabbj'))
+# print(e.calcEditDistance('abcabba', 'abcabbj'))
+# print('')
+# print(e.calcEditDistance('peace', 'people'))
