8561435d589aa85e5a530dc9fc3b6ed559b769a4
nyanmy <nyanmyth100@gmail.com>
2022-07-11 18:25:23 +0900

    upperCase

---
+++ upperChr.py
@@ -0,0 +1,20 @@
+def upper(abc):
+    ord(abc) - ord('a')
+    return ord(abc) - ord('a')
+
+def upperCase(Text):
+
+    text = list(Text)
+    print(text)
+
+    upperText = ''
+    
+    for c in text:
+        if  ord('a') <= ord(c) and ord(c) <= ord('z'):
+            upperText += chr(65 + upper(c))
+        else:
+            upperText += c
+
+    return upperText
+
+print(upperCase('Azあb'))
