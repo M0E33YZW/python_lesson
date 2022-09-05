3993dc09f8265566a94b0e49b092dc0293ac93f4
nyanmy <nyanmyth100@gmail.com>
2022-09-05 19:15:10 +0900

    省略

--- checkCharacter.py
+++ checkCharacter.py
@@ -1,4 +1,5 @@
 # modulus30
+N = 30
 inspection_dict = {'␣': 0, '.': 1, ',': 2, '?': 3}
 
 for i in range(26):
@@ -7,54 +8,46 @@ for i in range(26):
 
 
 def getValue(key):
-    value = inspection_dict[key]
-    return value
+    return inspection_dict[key]
 
 
 def getChar(num):
-    key = [k for k, v in inspection_dict.items() if v == num][0]
-    return key
+    return [k for k, v in inspection_dict.items() if v == num][0]
 
 
 def calcCheckCharacter(input):  # 検査文字の生成
-    N = 30
     sum = 0
     is_even = False
 
     i = len(input) - 1
     while i >= 0:
         value = getValue(input[i])
-        if is_even == True:
-            sum = sum + value
+        if is_even:
+            sum += value
         else:
-            sum = sum + (value*2) // N + (value*2) % N
+            sum += sum + (value*2) // N + (value*2) % N
         is_even = not is_even
         i -= 1
 
     check_value = (N - sum % N) % N
-    print(getChar(check_value))
     return getChar(check_value)  # 検査文字
 
 
 def validateCheckCharacter(input):  # 検査文字付文字列の検証
-    N = 30
     sum = 0
     is_odd = True
-    ret_value = True
 
     i = len(input) - 1
     while i >= 0:
         value = getValue(input[i])
-        if is_odd == True:
-            sum = sum + value
+        if is_odd:
+            sum += value
         else:
-            sum = sum + (value*2) // N + (value*2) % N
+            sum += sum + (value*2) // N + (value*2) % N
         is_odd = not is_odd
         i -= 1
 
-    if sum % N != 0:
-        ret_value = False
-
+    ret_value = sum % N != 0
     return ret_value
 
 
