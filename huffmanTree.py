b1490eeca12615ea2e5d35cc9b757dce84c22be4
nyanmy <nyanmyth100@gmail.com>
2022-10-04 10:26:47 +0900

    add Sort

--- huffmanTree.py
+++ huffmanTree.py
@@ -9,7 +9,7 @@ left = [-1 for _ in range(7)]
 # 右側の子
 right = [-1 for _ in range(7)]
 # 節の値
-freq = [10, 2, 4, 3, 5, 9, 19]
+freq = [10, 2, 4, 3]
 
 
 '''
@@ -17,7 +17,7 @@ freq = [10, 2, 4, 3, 5, 9, 19]
 '''
 def Huffman(size: int, parent: list[int], left: list[int],
             right: list[int], freq: list[int]):
-    node = []
+    node = [0 for _ in range(7)]
     nsize = 0
     SortNode(size, parent, freq, nsize, node)
     while nsize >= 2:
@@ -38,11 +38,12 @@ def Huffman(size: int, parent: list[int], left: list[int],
 その個数を変数 nsize に格納する。
 親が作成されていない節を表す要素組の要素番号を抽出し，節の値の昇順に整列する。
 '''
-def SortNode(size: int, parent: list[int], freq: list[int], nsize: int, node: list[int]):
+def SortNode(size: int, parent: list[int], freq: list[int],
+            nsize: int, node: list[int]):
     nsize = 0
     i = 0
     while i < size:
-        if parent[i] >= 0:
+        if parent[i] < 0:
             node[nsize] = i
             nsize += 1
         i += 1
@@ -53,11 +54,22 @@ def SortNode(size: int, parent: list[int], freq: list[int], nsize: int, node: li
 節を表す要素組の要素番号の配列 node を受け取り，
 要素番号に対応する要素組が表す節の値が昇順となるように整列する。
 節の値が同じときの順序は並べ替える直前の順序に従う。
+安定性補償のためにsorted()を使用。
+https://docs.python.org/3/library/functions.html#sorted
 '''
 def Sort(freq: list[int], nsize: int, node: list[int]):
-    print(nsize)
-    print(freq)
-    print(node)
+    print('freq', freq)
+    print('node', node)
+    dic = { key: val for key, val in zip(freq, node) }
+    print(dic)
+    dic2 = sorted(dic.items())
+    print(dic2)
+    node = []
+    freq = []
+    for i in dic2:
+        node.append(i[0])
+        freq.append(i[1])
+    print('node', node)
 
 
 def Encode(k: int, parent: list[int], left: list[int]):
